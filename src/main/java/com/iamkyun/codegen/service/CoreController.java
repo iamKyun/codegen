65ec7333a700542df36bd029efaa3c2b397e0218
kyun <me@iamkyun.com>
2024-11-11 13:22:17 +0800

    update

--- src/main/java/com/iamkyun/codegen/service/CoreController.java
+++ src/main/java/com/iamkyun/codegen/service/CoreController.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("api")
 public class CoreController {
     private final GenerationService generationService;
     private final TableService tableService;
